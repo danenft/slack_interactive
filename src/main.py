60251318d8b7f817b41967b6b2366c44aca3e992
anchangbae <dane@contxts.io>
2021-12-19 19:07:30 +0900

    기능별로 함수로 쪼개기

--- src/main.py
+++ src/main.py
@@ -20,24 +20,35 @@ class Main:
             text = f"동작 중 에러가 발생하였습니다: {e}"
             self.slack_bot.post_message(self.channel_id, text)
 
-    def _response_now_time(self):
+    def _response_now_time(self, message:dict) -> None:
         text = f"현재 시각은 {now_txt}입니다."
         self.slack_bot.post_thread_message(self.channel_id, message['ts'], text)
         return
 
-    def _response_redis_message_count(self):
+    def _response_redis_message_count(self, message:dict) -> None:
         redis_data_count = len(list(self.common.redis.scan_iter("*")))
         text = f"현재 redis에 적재된 메시지 수는 {redis_data_count:,}개 입니다."
+        self.slack_bot.post_thread_message(self.channel_id, message['ts'], text)
+
+    def _response_redis_truncate(self, message:dict) -> None:
+        redis_data = list(self.common.redis.scan_iter("*"))
+        redis_data_count = len(redis_data)
+        for key in redis_data:
+            self.common.redis.delete(key)
+        text = f"{redis_data_count}개의 데이터를 제거하였습니다"
         self.slack_bot.post_thread_message(self.channel_id, message['ts'], text)
 
     def execute_each_message(self):
         now_txt = datetime.now().strftime("%Y-%m-%d %H:%M")
 
         if message['text'] == "지금 시각은?":
-            self._response_now_time()
+            self._response_now_time(message)
 
         if message['text'] == "redis에 메시지 몇 개 적재됐어?":
-            self._response_redis_message_count()
+            self._response_redis_message_count(message)
+
+        if message['text'] == "redis 싹 비워!":
+            self._response_redis_truncate(message)
 
         self.common.to_redis(f"{message['user']}_{message['ts']}", now_txt)
 
